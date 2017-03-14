257e5fc127cc9923dee96e6f1101ea0e3cc2c6c2
Bobby Vu <bao050297@gmail.com>
2017-03-14 15:32:46 -0400

    Leave out side buttons

--- src/client/components/dumb/NoteEdit.js
+++ src/client/components/dumb/NoteEdit.js
@@ -7,10 +7,6 @@ import React from "react";
 export const NoteEdit = (props) => {
     return (
         <div className="card row offset-md-3 col-md-6">
-            <button type="button" className="close col-md-1" aria-label="Close">
-                <span className="float-left" aria-hidden="true">&laquo;</span>
-            </button>
-
             <div className="form-group">
                 <textarea
                     className="form-control"
@@ -30,7 +26,13 @@ export const NoteEdit = (props) => {
             </div>
         </div>
     );
-
 };
+
+
+
+// NEED FIX: add back button
+/*<button type="button" className="close col-md-1" aria-label="Close">
+ <span className="float-left" aria-hidden="true">&laquo;</span>
+ </button>*/
 
 
--- src/client/components/dumb/NoteView.js
+++ src/client/components/dumb/NoteView.js
@@ -7,9 +7,7 @@ export const NoteView = (props) => {
 
     return (
         <div className="card row offset-md-3 col-md-6">
-            <button type="button" className="close offset-md-11 col-md-1" aria-label="Close">
-                <span className="float-right" aria-hidden="true">&times;</span>
-            </button>
+
             <div className="card-block">
                 <h4 className="card-title">ID - {props.IdNumber}</h4>
                 <h6 className="card-subtitle mb-2 text-muted">Last Edited: {props.Date}</h6>
@@ -22,3 +20,7 @@ export const NoteView = (props) => {
 
 };
 
+// NEED FIX: add close button
+/*<button type="button" className="close offset-md-11 col-md-1" aria-label="Close">
+    <span className="float-right" aria-hidden="true">&times;</span>
+</button>*/
